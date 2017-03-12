6575a349a09b6913d36292d972bf89facc891d53
gamars <stephane@gamard.net>
2017-03-12 11:18:45 +0100

    updated js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   
   Pace.on('done',function(){
     
-  $('#viewport').show(1000)
+    $('#viewport').show(1000)
     $('#viewport').scrollbar();
     $('#splash').height($('#viewport').height())
     $('#outer-frame').height($('#outer-frame-ref').height())
@@ -10,19 +10,19 @@ $(document).ready(function(){
     $('#map').height($('#map-ref').height())
   
     $('.parallax-window').each(function() {
-    var off = $(this).offset().top
-    $(this).data('orig-offset', off);
-  });
-  
-  $('#viewport').not('.scroll-wrapper').on("scroll", function(){  
-    var scrollTop = $('#viewport').scrollTop();
-    $('.parallax-window').each(function(){
-      var off = $(this).data('orig-offset');
-      if (scrollTop >= off) {
-        var translate =  (scrollTop - off) / $(window).height() * 220;
-        $(this).css({transform: 'translateY(' + translate +'px)'});
-      }
+      var off = $(this).offset().top
+      $(this).data('orig-offset', off);
+ 
+      $('#viewport').not('.scroll-wrapper').on("scroll", function(){  
+        var scrollTop = $('#viewport').scrollTop();
+        $('.parallax-window').each(function(){
+          var off = $(this).data('orig-offset');
+          if (scrollTop >= off) {
+            var translate =  (scrollTop - off) / $(window).height() * 220;
+            $(this).css({transform: 'translateY(' + translate +'px)'});
+          }
+        });
+      });
     });
-  });
   });
 });
