0363d61bd3040a60d965f63977e8bc6f09796804
gamars <stephane@gamard.net>
2017-03-12 15:28:55 +0100

    reollback

--- js/app.js
+++ js/app.js
@@ -2,17 +2,21 @@ $(document).ready(function(){
   
   Pace.on('done',function(){
     $('#splash').height($(window).height())
-
     $('.home-image').fadeIn()
     $('#outer-frame').height($('#outer-frame-ref').height())
     $('#ceremony-frame').height($('#ceremony-ref').height())
     $('#map').height($('#map-ref').height())
   
+    $('.parallax-window').each(function() {
+      var off = $(this).offset().top
+      $(this).data('orig-offset', off);
+    });
     
     $(window).on("scroll", function(){
       var scrollTop = $(window).scrollTop();
-      var translate =  scrollTop / $(window).height() * 220;
-      $('.parallax-window').each(function(){
+      $('.parallax-window').each(function(){        
+          var off = $(this).data('orig-offset');
+          var translate =  (scrollTop - off) / $(window).height() * 220;
           $(this).css({transform: 'translateY(' + translate +'px)'});
       });
     });
