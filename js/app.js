a5360301082416adae96f996565ce4e64c1d0b41
gamars <stephane@gamard.net>
2017-03-12 16:19:30 +0100

    faster parallax

--- js/app.js
+++ js/app.js
@@ -1,23 +1,17 @@
 $(document).ready(function(){
   
   Pace.on('done',function(){
-    $('#splash').height($(window).height())
-    $('.home-image').fadeIn()
-    $('#outer-frame').height($('#outer-frame-ref').height())
-    $('#ceremony-frame').height($('#ceremony-ref').height())
-    $('#map').height($('#map-ref').height())
+    $('#splash').height($(window).height());
+    $('.home-image').fadeIn();
+    $('#outer-frame').height($('#outer-frame-ref').height());
+    $('#ceremony-frame').height($('#ceremony-ref').height());
+    $('#map').height($('#map-ref').height());
   
-    $('.parallax-window').each(function() {
-      var off = $(this).offset().top
-      $(this).data('orig-offset', off);
-    });
-    
-    $(window).on("scroll", function(){
+    $(window).on("scroll", function(event){
       var scrollTop = $(window).scrollTop();
-      $('.parallax-window').each(function(){        
-          var off = $(this).data('orig-offset');
-          var translate =  (scrollTop - off) / $(window).height() * 220;
-          $(this).css({transform: 'translateY(' + translate +'px)'});
+      $('.parallax-window').each(function(){
+        var delta = (scrollTop-($(this).offset().top)) * 0.2
+        $(this).css({transform: 'translateY(' + delta +'px)'});    
       });
     });
   });
